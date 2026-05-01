90d8d0a6915319b72b5504f57241ce0b3d2bf122
Shounak-ctrl <shounak15july2004@gmail.com>
2026-05-01 14:21:27 +0530

    test code

---
+++ test2.dart
@@ -0,0 +1,11 @@
+void main() {
+  String username = "Shounak";
+  print("Welcome user $username"); 
+    int number=25;
+    if (number%2==0 ) {
+      print("The number is even");
+    } else {
+      print("The number is odd");
+    }
+  }
+
